7af8d1dabc7f84f213aa885314f94a60ab50710f
HoffmanSan <133865170+HoffmanSan@users.noreply.github.com>
2024-06-12 10:25:09 +0200

    Update templateEN.js

--- src/pages/templateEN.js
+++ src/pages/templateEN.js
@@ -4,7 +4,7 @@ export default function templateEN() {
     <>
     <div className="grid w-10/12 grid-cols-3 gap-5 mx-auto my-5">
     	{/* CARD */}
-				<div className="order-3 w-1/3 card">
+				<div className="order-3 card">
 						
 						<h2>HealThyBody</h2>
 						<p className="my-2">An application for managing daily calorie and macronutriens intake.</p>
